8e1a3bfb6d6e23d5cfdcf8291a9b51a381129676
Michael Bikovitsky <biko@bikodbg.com>
2021-09-24 13:50:10 +0300

    Add about screen placeholder

---
+++ app/src/main/java/com/bikodbg/sharetopinboard/AboutFragment.kt
@@ -0,0 +1,5 @@
+package com.bikodbg.sharetopinboard
+
+import androidx.fragment.app.Fragment
+
+class AboutFragment : Fragment(R.layout.fragment_about)
--- app/src/main/java/com/bikodbg/sharetopinboard/SettingsActivity.kt
+++ app/src/main/java/com/bikodbg/sharetopinboard/SettingsActivity.kt
@@ -2,9 +2,12 @@ package com.bikodbg.sharetopinboard
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 
-class SettingsActivity : AppCompatActivity() {
+class SettingsActivity : AppCompatActivity(),
+    PreferenceFragmentCompat.OnPreferenceStartFragmentCallback {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
@@ -14,6 +17,22 @@ class SettingsActivity : AppCompatActivity() {
                 .replace(R.id.settings, SettingsFragment())
                 .commit()
         }
+    }
+
+    override fun onPreferenceStartFragment(
+        caller: PreferenceFragmentCompat,
+        pref: Preference
+    ): Boolean {
+        val fragment =
+            supportFragmentManager.fragmentFactory.instantiate(classLoader, pref.fragment)
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.settings, fragment)
+            .addToBackStack(null)
+            .commit()
+
+        return true
     }
 
     class SettingsFragment : PreferenceFragmentCompat() {
